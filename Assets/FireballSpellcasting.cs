a44c526ba8eb17c1ab0e5144ca3ab011cb5c4a72
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-10-17 14:46:06 +0300

    Added fireball sprites for all directions.
    Added fireball sprites for all directions, pixels per unit is the same across all sprites.

---
+++ Assets/FireballSpellcasting.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballSpellcasting : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fireball casting animation
+        if (Input.GetKey(KeyCode.F))
+        {
+            animator.SetBool("CastingFireball", true);
+        }
+    }
+}
