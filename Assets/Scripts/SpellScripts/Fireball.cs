374e63ef89457524bfc616b7a7b0e31ab65c774b
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-10-19 11:40:30 +0300

    Created a fireball script in the proper directory.
    Created a fireball script in the proper directory, changed the fireball prefab to have a rigidbody and linked the script to it.

---
+++ Assets/Scripts/SpellScripts/Fireball.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float fireballSpeed;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Position the fireball in the appropriate direction and calculate its velocity
+    void Setup(Vector2 velocity, Vector3 direction)
+    {
+        rb.velocity = velocity.normalized * fireballSpeed;
+
+        // Find out a 2D object's direction
+        transform.rotation = Quaternion.Euler(direction);
+    }
+
+}
