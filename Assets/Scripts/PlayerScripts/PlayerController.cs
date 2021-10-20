eea940b7261abe2db56332dd5e936d850d80915d
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-10-20 17:03:32 +0300

    Improved fireball casting.
    Improved fireball casting, made it so that the fireball is conjured after the casting animation is complete, modified existing fireball velocity calculation.

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -35,28 +35,29 @@ public class PlayerController : MonoBehaviour
     // Wait a delay after casting a fireball before moving
     private IEnumerator WaitForFireballAnimation()
     {
+        // In order to enable diagonal casting we must save the player's state before the casting animation
+        //  disables control
+        Vector2 fireballVelocityBeforeAnimation = movement.GetDirectionVelocity();
         animator.SetBool("IsCasting", true);
         movement.SetControlEnabled(false);
 
         yield return new WaitForSeconds(fireballCastAnimationDuration);
 
-        CreateFireball();
+        CreateFireball(fireballVelocityBeforeAnimation);
         animator.SetBool("IsCasting", false);
         movement.SetControlEnabled(true);
     }
 
     // Creates a fireball at the appropriate location near the player with a computed velocity and angle
-    private void CreateFireball()
+    private void CreateFireball(Vector2 fireballVelocity)
     {
-        Vector2 fireballVelocity = movement.GetDirectionVelocity();
         Fireball fireball = Instantiate(fireballProjectile, transform.position, Quaternion.identity).GetComponent<Fireball>();
-        fireball.Setup(fireballVelocity, ComputeFireballAngle());
+        fireball.Setup(fireballVelocity, ComputeFireballAngle(fireballVelocity));
     }
 
     // Calculates at what angle should the fireball sprite fly
-    private Vector3 ComputeFireballAngle()
+    private Vector3 ComputeFireballAngle(Vector2 fireballVelocity)
     {
-        Vector2 fireballVelocity = movement.GetDirectionVelocity();
         // Calculate the rotation angle in degrees
         float degrees = Mathf.Atan2(fireballVelocity.y, fireballVelocity.x) * Mathf.Rad2Deg;
         return new Vector3(0, 0, degrees);
