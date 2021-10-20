fc7e014d4035ea735b50b6330fb2ba872dd2319a
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-10-20 17:53:00 +0300

    Added flying animation for new fireball sprite.
    Added flying animation for new fireball sprite and the required states.

--- Assets/Scripts/SpellScripts/Fireball.cs
+++ Assets/Scripts/SpellScripts/Fireball.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    public Animator animator;
     public float fireballSpeed;
+    public float fireballExplosionAnimationDuration;
+    public Vector3 currentDirection;
     public Rigidbody2D rb;
+
+    // Awake is called when the object is created, but before Start
+    // If we don't set the IsFlying value here, the object will never stay in the fireball_fly state
+    //  because by default the boolean value at creation won't be true or false
+    void Awake()
+    {
+        animator.SetBool("IsFlying", true);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Position the fireball in the appropriate direction and calculate its velocity
     public void Setup(Vector2 velocity, Vector3 direction)
     {
         rb.velocity = velocity.normalized * fireballSpeed;
-
+        // Rememebr the direction of the fireball for when we need to
+        currentDirection = direction;
         // Find out a 2D object's direction
         transform.rotation = Quaternion.Euler(direction);
     }
@@ -28,8 +40,18 @@ public class Fireball : MonoBehaviour
         // Destroy the fireball object if it collides with anything except the player or the grid
         if (!otherObject.gameObject.CompareTag("Player") && !otherObject.gameObject.CompareTag("Grid"))
         {
+            StartCoroutine(WaitForExplosionAnimation());
             Destroy(this.gameObject);
         }
+    }
+
+    private IEnumerator WaitForExplosionAnimation()
+    {
+        animator.SetBool("IsFlying", false);
+        // Stop the fireball but keep its rotation the same as previously
+        Setup(new Vector2(0, 0), currentDirection);
+
+        yield return new WaitForSeconds(fireballExplosionAnimationDuration);
     }
 
 }
