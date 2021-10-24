e25576b9c02d4e79b3acd00de5f251bbe38a97ec
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-10-24 18:09:55 +0300

    Moved all logic to ForestZone and deleted SampleScene.

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -8,17 +8,125 @@ public class PlayerController : MonoBehaviour
     public PlayerMovement movement;
     public float attackAnimationDuration = 0f;
     public GameObject fireballProjectile;
+    public float fireballManaCost;
     public float fireballCastAnimationDuration = 0f;
+    public float attackStaminaCost;
+    public float currentHealth;
+    public float maxHealth;
+    public HealthBar healthBar;
+    public float currentMana;
+    public float maxMana;
+    public ManaBar manaBar;
+    public float currentStamina;
+    public float maxStamina;
+    public StaminaBar staminaBar;
+    public bool playerIsDead;
+    
+    void Awake()
+    {
+        playerIsDead = false;
+    }
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        currentMana = maxMana;
+        currentStamina = maxStamina;
+        UpdateMaxHealth(maxHealth);
+        UpdateMaxMana(currentMana);
+        UpdateMaxStamina(currentStamina);
+    }
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1") && !animator.GetBool("IsAttacking") && !animator.GetBool("IsCasting")) {
+            UseStamina(attackStaminaCost);
             StartCoroutine(WaitForAttackAnimation());
         }
         else if (Input.GetButtonDown("Spell1") && !animator.GetBool("IsAttacking"))
         {
+            UseMana(fireballManaCost);
             StartCoroutine(WaitForFireballAnimation());
         }
+    }
+
+     // Update health bar value.
+    void UpdateHealth(float health)
+    {
+        healthBar.SetHealth(health);
+    }
+
+    // Set max health value in health bar and update health bar current health value.
+    void UpdateMaxHealth(float maxHealth)
+    {
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update mana bar value.
+    void UpdateMana(float mana)
+    {
+        manaBar.SetMana(mana);
+    }
+
+    // Set max mana value in mana bar and update mana bar current mana value.
+    void UpdateMaxMana(float maxMana)
+    {
+        manaBar.SetMaxMana(maxMana);
+    }
+
+    // Update stamina bar value.
+    void UpdateStamina(float stamina)
+    {
+        staminaBar.SetStamina(stamina);
+    }
+
+    // Set max stamina value in stamina bar and update stamina bar current stamina value.
+    void UpdateMaxStamina(float maxStamina)
+    {
+        staminaBar.SetMaxStamina(maxStamina);
+    }
+
+    // Take damage and lose health.
+    void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0)
+        {
+            // If health is below zero, the player is dead and health is set to zero.
+            playerIsDead = true;
+            currentHealth = 0f;
+        }
+
+        // Update UI after taking damage.
+        UpdateHealth(currentHealth);
+    }
+
+    // Use mana resource.
+    void UseMana(float manaUsed)
+    {
+        currentMana -= manaUsed;
+        if (currentMana < 0)
+        {
+            // If mana is below zero, it is set to zero.
+            currentMana = 0f;
+        }
+
+         // Update UI after using mana.
+        UpdateMana(currentMana);
+    }
+
+    // Use stamina resource.
+    void UseStamina(float staminaUsed)
+    {
+        currentStamina -= staminaUsed;
+        if (currentStamina < 0)
+        {
+            // If stamina is below zero, it is set to zero.
+            currentStamina = 0f;
+        }
+
+        // Update UI after using stamina.
+        UpdateStamina(currentStamina);
     }
 
     private IEnumerator WaitForAttackAnimation() 
