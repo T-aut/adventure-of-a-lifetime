6e599fd81de4347c42e20a05ca36f51585472cf1
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-10-24 17:17:48 +0300

    Renamed player prefab and script to not have to deal with merge conflicts.

--- Assets/Scripts/PlayerScripts/RscPlayerController.cs
+++ Assets/Scripts/PlayerScripts/RscPlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class RscPlayerController : MonoBehaviour
 {
     public Animator animator;
     public PlayerMovement movement;
