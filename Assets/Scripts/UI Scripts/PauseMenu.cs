021d7bd517e3f809e37d3cd58b9d7dd61841baa5
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-10-26 10:58:04 +0300

    Merge my changes with ForestZone scene.

---
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject PauseMenuUI;
+    public PlayerMovement PlayerMovement;
+
+    void Awake()
+    {
+        PauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (GamePaused) ResumeGame();
+            else PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        GamePaused = true;
+
+        PlayerMovement.SetControlEnabled(false);
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        GamePaused = false;
+
+        PlayerMovement.SetControlEnabled(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
