ec2115c403afc3b242eb6a92244bb66ec0ecfe54
Algirdas Benetis <Algirdas.Benetis@Gmail.com>
2021-11-14 23:11:28 +0200

    Scenu perėjimas (reikia nustatyt zaidėjo pozicija)
    Yra padaryti perėjimai ant scenų be žaidėjo pozicijos nustatymo. Kaip komentaras pvz. pateiktas perėjimas, kai paspaudžiamas pelės mygtukas.

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Turetu but if(ZAIDEJAS ATITINKAMOJ SCENOS VIETOJ)
+        //pvz.: if (Input.GetMouseButtonDown(0)) - paspaudziamas pelës mygtukas
+        if (ZAIDEJAS ATITINKAMOJ SCENOS VIETOJ)
+            {
+                LoadNextLevel();
+            }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
