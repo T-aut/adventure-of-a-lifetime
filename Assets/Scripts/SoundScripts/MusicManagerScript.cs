007419ad8c0b760f5e8939e2bb4987c4ceeadbdd
bernardas.kukulskis <bernardas.kukulskis@devbridge.com>
2021-11-24 00:03:34 +0200

    Sounds castle theme

--- Assets/Scripts/SoundScripts/MusicManagerScript.cs
+++ Assets/Scripts/SoundScripts/MusicManagerScript.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+ using System.Collections;
+ using UnityEngine.SceneManagement;
 
 public class MusicManagerScript : MonoBehaviour
 {
-    public static AudioClip forestMusic;
+    public static AudioClip forestMusic, castleMusic;
     static AudioSource audioSource; 
+    public string sceneName;
     // Start is called before the first frame update
     void Start()
     {
+        Scene currentScene = SceneManager.GetActiveScene ();
+        sceneName = currentScene.name;
+
         forestMusic = Resources.Load<AudioClip> ("Audio/Music/forestMusic");
+        castleMusic = Resources.Load<AudioClip> ("Audio/Music/CastleMusic");
+
 
         audioSource = GetComponent<AudioSource> ();  
 
@@ -16,12 +24,25 @@ public class MusicManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        if(!PauseMenu.GamePaused){
-            if(!audioSource.isPlaying){
-                audioSource.PlayOneShot(forestMusic);
+        if (sceneName == "ForestZone") {
+            if(!PauseMenu.GamePaused){
+                if(!audioSource.isPlaying){
+                    audioSource.PlayOneShot(forestMusic);
+                }
+            }else{
+                audioSource.Stop();
             }
-        }else{
-            audioSource.Stop();
         }
+        else if(sceneName == "CastleZone")
+        {
+            if(!PauseMenu.GamePaused){
+                if(!audioSource.isPlaying){
+                    audioSource.PlayOneShot(castleMusic);
+                }
+            }else{
+                audioSource.Stop();
+            }
+        }
+        
     }
 }
