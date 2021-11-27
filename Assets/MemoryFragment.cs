d780dab7ceb6d53ae9556467128d3444723fb4f2
Tautvydas Jackevičius <tautvydas.jackevicius@gmail.com>
2021-11-27 19:34:58 +0200

    Bad implementation of the dialog and altar unlocking :)

--- Assets/MemoryFragment.cs
+++ Assets/MemoryFragment.cs
@@ -5,10 +5,36 @@ using UnityEngine;
 public class MemoryFragment : MonoBehaviour
 {
     public Animator animator;
+    public GameObject Dialog1;
+    public GameObject Dialog2;
+    public PlayerMovement PlayerMovement;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (!animator.GetBool("IsActivated")) animator.SetBool("IsActivated", true);
         this.gameObject.GetComponent<BoxCollider2D>().enabled = false; // so that the character does not collide with the fading fragment
+
+        Dialog1.SetActive(true);
+        PlayerMovement.SetControlEnabled(false);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Dialog1.activeSelf)
+            {
+                Dialog1.SetActive(false);
+                Dialog2.SetActive(true);
+                return;
+            }
+            if (Dialog2.activeSelf)
+            {
+                Dialog2.SetActive(false);
+                PlayerMovement.SetControlEnabled(true);
+                PlayerMovement.SetAltarUnlocked(true);
+                return;
+            }
+        }
     }
 }
